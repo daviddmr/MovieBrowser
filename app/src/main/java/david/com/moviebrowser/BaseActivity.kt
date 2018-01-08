d6fc395c3267bfca54c1d0ffcd00e524ca602437
David Ribeiro <david.dmr@gmail.com>
2018-01-08 12:09:27 -0300

    Cleaning of uselles stuffs in the mainActivity | Refactor of the name of 'MainActivity' to 'BaseActivity'

--- app/src/main/java/david/com/moviebrowser/BaseActivity.kt
+++ app/src/main/java/david/com/moviebrowser/BaseActivity.kt
@@ -1,7 +1,6 @@
 package david.com.moviebrowser
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
@@ -11,17 +10,12 @@ import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class BaseActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
 
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
@@ -40,40 +34,28 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
         return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
-        }
-    }
-
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
         when (item.itemId) {
-            R.id.nav_camera -> {
-                // Handle the camera action
-            }
-            R.id.nav_gallery -> {
+            R.id.nav_movies -> {
 
             }
-            R.id.nav_slideshow -> {
+            R.id.nav_movies_by_genre -> {
 
             }
-            R.id.nav_manage -> {
+            R.id.nav_favorites -> {
 
             }
-            R.id.nav_share -> {
+            R.id.nav_top10 -> {
 
             }
-            R.id.nav_send -> {
+            R.id.nav_register -> {
+
+            }
+            R.id.nav_logout -> {
 
             }
         }
