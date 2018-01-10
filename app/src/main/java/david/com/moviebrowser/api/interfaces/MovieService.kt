16b44064f5f3228cbf8ade9e0f02520ab9c00b75
David Ribeiro <david.dmr@gmail.com>
2018-01-10 11:47:11 -0300

    Changing request getAllMovies to getTopRatedMovies

--- app/src/main/java/david/com/moviebrowser/api/interfaces/MovieService.kt
+++ app/src/main/java/david/com/moviebrowser/api/interfaces/MovieService.kt
@@ -1,17 +1,19 @@
 package david.com.moviebrowser.api.interfaces
 
 import david.com.moviebrowser.model.ResponseBody
+import david.com.moviebrowser.util.Constants.Companion.API_VERSION_3
+import david.com.moviebrowser.util.Constants.Companion.movie
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface MovieService {
 
-    @GET("list_movies.json")
-    fun getAllMovies(
-            @Query("limit") limit: Int,
+    @GET(API_VERSION_3 + movie + "top_rated")
+    fun getTopRatedMovies(
             @Query("page") page: Int,
-            @Query("genre") genre: String?
+            @Query("api_key") apiKey: String,
+            @Query("language") language: String?
     ): Call<ResponseBody>
 
 }
