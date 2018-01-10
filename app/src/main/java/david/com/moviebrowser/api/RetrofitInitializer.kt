f2d3db6b7050bc892973300fc87da8c3afef2f15
David Ribeiro <david.dmr@gmail.com>
2018-01-10 11:45:12 -0300

    Including httplogginginterceptor on retrofit requests

--- app/src/main/java/david/com/moviebrowser/api/RetrofitInitializer.kt
+++ app/src/main/java/david/com/moviebrowser/api/RetrofitInitializer.kt
@@ -2,14 +2,27 @@ package david.com.moviebrowser.api
 
 import david.com.moviebrowser.api.interfaces.MovieService
 import david.com.moviebrowser.util.Constants.Companion.BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class RetrofitInitializer {
 
+    private var logging: HttpLoggingInterceptor = HttpLoggingInterceptor()
+    private var httpClient: OkHttpClient.Builder
+
+    init {
+        logging.level = HttpLoggingInterceptor.Level.BASIC
+
+        httpClient = OkHttpClient.Builder()
+        httpClient.addInterceptor(logging)
+    }
+
     private val retrofit = Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
             .baseUrl(BASE_URL)
+            .client(httpClient.build())
             .build()
 
     fun movieService(): MovieService = retrofit.create(MovieService::class.java)
