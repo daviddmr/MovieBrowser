130187216206799677e31245aa95116ee9c95ce1
David Ribeiro <david.dmr@gmail.com>
2018-01-10 15:43:16 -0300

    Setting application class to initialize Realm configurations

--- app/src/main/java/david/com/moviebrowser/application/MovieBrowserApplication.kt
+++ app/src/main/java/david/com/moviebrowser/application/MovieBrowserApplication.kt
@@ -1,9 +1,21 @@
 package david.com.moviebrowser.application
 
 import android.app.Application
+import io.realm.Realm
+import io.realm.RealmConfiguration
 
 class MovieBrowserApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        Realm.init(this)
+        val realmConfig = RealmConfiguration
+                .Builder()
+                .name("movie_browser.realm")
+                .schemaVersion(1)
+                .build()
+
+        Realm.setDefaultConfiguration(realmConfig)
+
     }
 }
