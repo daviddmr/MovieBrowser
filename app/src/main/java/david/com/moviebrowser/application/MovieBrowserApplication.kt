7446f06b1637b34fda14709e06e006a6ea11ed8b
David Ribeiro <david.dmr@gmail.com>
2018-01-10 15:35:14 -0300

    Creating Application class

---
+++ app/src/main/java/david/com/moviebrowser/application/MovieBrowserApplication.kt
@@ -0,0 +1,9 @@
+package david.com.moviebrowser.application
+
+import android.app.Application
+
+class MovieBrowserApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
