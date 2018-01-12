42de8612e6554d116038cec381c9981e8f857f72
David <david.dmr@gmail.com>
2018-01-12 02:42:52 -0300

    Setting recyclerview to show top rated movies list

--- app/src/main/java/david/com/moviebrowser/fragments/MovieFragment.kt
+++ app/src/main/java/david/com/moviebrowser/fragments/MovieFragment.kt
@@ -2,17 +2,21 @@ package david.com.moviebrowser.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import david.com.moviebrowser.R
+import david.com.moviebrowser.adapters.MovieTopRatedAdapter
 import david.com.moviebrowser.api.RetrofitInitializer
 import david.com.moviebrowser.model.Movie
 import david.com.moviebrowser.model.ResponseBody
 import david.com.moviebrowser.util.Constants.Companion.API_KEY
 import david.com.moviebrowser.util.Constants.Companion.language
 import io.realm.Realm
+import kotlinx.android.synthetic.main.fragment_movies.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -36,20 +40,35 @@ class MovieFragment : Fragment() {
         loadMovies()
     }
 
+    fun initRecyclerView(movies: List<Movie>) {
+        rvTopRatedMovies.setHasFixedSize(true)
+        val linearLayoutManager = LinearLayoutManager(context)
+        rvTopRatedMovies.layoutManager = linearLayoutManager
+
+        val dividerItemDecoration = DividerItemDecoration(context, linearLayoutManager.orientation)
+        rvTopRatedMovies.addItemDecoration(dividerItemDecoration)
+
+        val movieTopRatedAdapter = MovieTopRatedAdapter(context, movies)
+        rvTopRatedMovies.adapter = movieTopRatedAdapter
+    }
+
     private fun loadMovies() {
 
         val realm = Realm.getDefaultInstance()
 
         val call = RetrofitInitializer().movieService().getTopRatedMovies(1, API_KEY, language)
         call.enqueue(object : Callback<ResponseBody> {
-            override fun onResponse(call: Call<ResponseBody>?, response: Response<ResponseBody>) {
+            override fun onResponse(call: Call<ResponseBody>?, response: Response<ResponseBody>?) {
                 Log.d("EX", "")
-                realm.beginTransaction()
-                realm.copyToRealmOrUpdate(response.body()?.movies)
-                realm.commitTransaction()
+                val movies: List<Movie> = response?.body()?.movies ?: listOf()
+                movies.isNotEmpty().let {
+                    initRecyclerView(movies)
+                    realm.executeTransaction {
+                        realm.copyToRealmOrUpdate(movies)
+                    }
+                }
 
-                val movie = realm.where(Movie::class.java).findAll()
-                val movie2 = realm.copyFromRealm(movie)
+                val moviesQueried: List<Movie> = realm.copyFromRealm(realm.where(Movie::class.java).findAll())
                 Log.d("EX", "")
             }
 
