7cbe07ccc8680af35fd673386fa29cf581d677ae
David Ribeiro <david.dmr@gmail.com>
2018-01-17 14:33:40 -0300

    Create a blank login fragment

---
+++ app/src/main/java/david/com/moviebrowser/fragments/LoginFragment.kt
@@ -0,0 +1,31 @@
+package david.com.moviebrowser.fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import david.com.moviebrowser.R
+
+class LoginFragment : Fragment() {
+
+    companion object {
+        val TAG = "LoginFrag"
+
+        fun newInstance(): LoginFragment {
+            return LoginFragment()
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d(TAG, "")
+    }
+
+}
