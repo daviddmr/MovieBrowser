4e2728d882f9413b905336a1b6531f8115d4df9e
David <david.dmr@gmail.com>
2018-01-17 20:39:13 -0300

    Removing tabLayout and ViewPager of Login Screen

--- app/src/main/java/david/com/moviebrowser/activities/LoginActivity.kt
+++ app/src/main/java/david/com/moviebrowser/activities/LoginActivity.kt
@@ -8,25 +8,9 @@ import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
-    private val contentViewPagerAdapter: ContentViewPagerAdapter = ContentViewPagerAdapter(supportFragmentManager, this)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
-        initComponents()
     }
 
-    private fun initComponents() {
-        vpLoginContent.adapter = contentViewPagerAdapter
-        tlLoginOptions.setupWithViewPager(vpLoginContent)
-        updateTabs()
-    }
-
-    private fun updateTabs() {
-        for (i in 0..tlLoginOptions.tabCount) {
-            val tab = tlLoginOptions.getTabAt(i)
-            tab?.customView = contentViewPagerAdapter.getTabView(i)
-        }
-    }
 }
