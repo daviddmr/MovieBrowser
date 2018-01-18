265a4701cb1ae844b0c04971c62cb7d5c9347971
David <david.dmr@gmail.com>
2018-01-17 21:47:42 -0300

    Implementation of listeners to button that change visibility of password

--- app/src/main/java/david/com/moviebrowser/activities/LoginActivity.kt
+++ app/src/main/java/david/com/moviebrowser/activities/LoginActivity.kt
@@ -1,9 +1,13 @@
 package david.com.moviebrowser.activities
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.view.View
 import david.com.moviebrowser.R
-import david.com.moviebrowser.adapters.ContentViewPagerAdapter
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
@@ -11,6 +15,45 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        etPassword.addTextChangedListener(textChangedListener())
+        ibPasswordVisibility.setOnClickListener(onClickListenerVisibility())
+    }
+
+    private fun textChangedListener(): TextWatcher {
+        return object : TextWatcher {
+            override fun afterTextChanged(editable: Editable) {
+                if (editable.isNotEmpty()) {
+                    ibPasswordVisibility.visibility = View.VISIBLE
+                    ibPasswordVisibility.isEnabled = true
+                } else {
+                    ibPasswordVisibility.visibility = View.GONE
+                    ibPasswordVisibility.isEnabled = false
+                }
+            }
+
+            override fun beforeTextChanged(charSequence: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(charSequence: CharSequence?, start: Int, before: Int, count: Int) {}
+        }
+    }
+
+    private var passwordDisplayed = false
+
+    private fun onClickListenerVisibility(): View.OnClickListener {
+        return View.OnClickListener {
+            if (passwordDisplayed) {
+                etPassword.transformationMethod = PasswordTransformationMethod.getInstance()
+                etPassword.setSelection(etPassword.text.length)
+                ibPasswordVisibility.setImageResource(R.drawable.ic_visibility_off_black_24dp)
+                passwordDisplayed = false
+            } else {
+                etPassword.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                etPassword.setSelection(etPassword.text.length)
+                ibPasswordVisibility.setImageResource(R.drawable.ic_visibility_black_24dp)
+                passwordDisplayed = true
+            }
+        }
     }
 
 }
